a2a8cf3b9176e1d5b9a6ecba114a71d622890dca
Santiago Mesa Parra <86623637+SantiagoMesa0911@users.noreply.github.com>
2025-05-08 17:29:57 -0500

    creation of services and routes

--- Backend/Models/VehiculoMol.js
+++ Backend/Models/VehiculoMol.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose')
 
-const VehiculoSchema = new  mongoose.Schema({
-    placa:{
+const VehiculoSchema = new mongoose.Schema({
+    placa: {
         type: String,
         required: true,
-        unique:true
+        unique: true
     },
-    tipo:{
-        type:String,
-        required:true
-    }
-})
+    tipo: {
+        type: String,
+        required: true
+    },
+    horaEntrada: { type: Date, required: true },
+    horaSalida: { type: Date, default: null },
+}, {
+    timestamps: true,
+});
 
 
 const Vehiculo = mongoose.model('Vehiculo', VehiculoSchema)
