ed2f266168be89bd7c43cc484be11602bf9c0ad3
Santiago Mesa Parra <86623637+SantiagoMesa0911@users.noreply.github.com>
2025-05-09 23:03:29 -0500

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,1 +1,1 @@
-export const environment = { production: true, apiUrl: 'https://autos-colombia-backend.vercel.app/' }; 
+export const environment = { production: true, apiUrl: 'https://autos-colombia-backend.vercel.app' }; 
